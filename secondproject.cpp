525ff9ee77054c6eb6cc5dded366fc6bfa54158e
Kapuramani77 <66730123+Kapuramani77@users.noreply.github.com>
2020-07-09 18:12:46 +0530

    product
    It shows the product of two numbers.

---
+++ secondproject.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double a , b, product;
+    cout<<"Enter two numbers:";
+    cin>>a;
+    cin>>b;
+
+    product=(a*b);
+    cout<<"product is :"<<product;
+    return 0;
+}
